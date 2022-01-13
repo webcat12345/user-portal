8bdcdd19841098f09da67cf188b4c0e968af4546
webcat12345 <rango0986@gmail.com>
2022-01-13 05:34:00 -0800

    test: update karam config to run test on headless

--- karma.conf.js
+++ karma.conf.js
@@ -35,10 +35,16 @@ module.exports = function (config) {
     reporters: ['progress', 'kjhtml'],
     port: 9876,
     colors: true,
-    logLevel: config.LOG_INFO,
-    autoWatch: true,
-    browsers: ['Chrome'],
-    singleRun: false,
-    restartOnFileChange: true
+    logLevel: config.LOG_DISABLE,
+    autoWatch: false,
+    browsers: ['ChromeHeadlessNoSandbox'],
+    customLaunchers: {
+      ChromeHeadlessNoSandbox: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox']
+      }
+    },
+    singleRun: true,
+    restartOnFileChange: false
   });
 };
