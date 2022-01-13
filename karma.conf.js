b83d664135175b13a21e53dbfdbc8f7c1c4498e0
webcat12345 <rango0986@gmail.com>
2022-01-13 07:15:20 -0800

    feat: generate coverage report in json

--- karma.conf.js
+++ karma.conf.js
@@ -25,11 +25,12 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/user-portal'),
+      dir: require('path').join(__dirname, './coverage'),
       subdir: '.',
       reporters: [
         { type: 'html' },
-        { type: 'text-summary' }
+        { type: 'text-summary' },
+        { type: 'json' }
       ]
     },
     reporters: ['progress', 'kjhtml'],
