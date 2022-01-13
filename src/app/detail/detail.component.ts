b05bf19d38347f74db7a83b3d51798731f66e628
webcat12345 <rango0986@gmail.com>
2022-01-13 04:36:24 -0800

    chore: update change detection strategy for performance improve

--- src/app/detail/detail.component.ts
+++ src/app/detail/detail.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 import { DetailStateService } from '../shared/detail-state/detail-state.service';
 
 @Component({
   selector: 'app-detail',
   templateUrl: './detail.component.html',
-  styleUrls: ['./detail.component.scss']
+  styleUrls: ['./detail.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class DetailComponent {
 
--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'app-layout',
   templateUrl: './layout.component.html',
-  styleUrls: ['./layout.component.scss']
+  styleUrls: ['./layout.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class LayoutComponent {
 
