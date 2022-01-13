9d926d3a77e29bb2335396e41d1fa4d554d1871b
webcat12345 <rango0986@gmail.com>
2022-01-12 23:07:58 -0800

    feat: navigate to detail page when item found

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { finalize } from 'rxjs';
 
 import { UserService } from '../core/services/user.service';
 
@@ -10,18 +12,31 @@ import { UserService } from '../core/services/user.service';
 })
 export class HomeComponent implements OnInit {
 
+  isLoading = false;
   form: FormGroup = this.fb.group({
     id: ['', [Validators.required, Validators.pattern('^[0-9]*$')]]
   });
 
   constructor(
     private fb: FormBuilder,
-    private userService: UserService
+    private userService: UserService,
+    private router: Router
   ) { }
 
   ngOnInit(): void {
   }
 
   search() {
+    if (this.form.invalid) {
+      return;
+    }
+    const id = this.form.value.id;
+
+    this.isLoading = true;
+    this.userService.getUserById(id).pipe(
+      finalize(() => this.isLoading = false)
+    ).subscribe(user => {
+      this.router.navigate([this.form.value.id] );
+    });
   }
 }
