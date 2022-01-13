47fa9f953a52abef47750b9cbfe44d35770ff7a1
webcat12345 <rango0986@gmail.com>
2022-01-12 21:58:47 -0800

    feat: default routing architecture

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: ':id', loadChildren: () => import('./detail/detail.module').then(m => m.DetailModule) }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
