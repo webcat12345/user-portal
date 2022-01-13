018dc8d05cded56e2d55c73f6d25ab11375d4232
webcat12345 <rango0986@gmail.com>
2022-01-13 08:30:43 -0800

    test: detail state service unit test

--- src/app/shared/detail-state/detail-state.service.spec.ts
+++ src/app/shared/detail-state/detail-state.service.spec.ts
@@ -2,6 +2,7 @@ import { TestBed } from '@angular/core/testing';
 
 import { DetailStateService } from './detail-state.service';
 import { DetailStateModule } from './detail-state.module';
+import { users } from '../../mock/user.mock';
 
 describe('DetailStateService', () => {
   let service: DetailStateService;
@@ -17,5 +18,16 @@ describe('DetailStateService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should return null when user is not set', () => {
+    const response = service.getUser();
+    expect(response).toBe(null);
+  });
+
+  it('should set user once it is set', () => {
+    service.setUser(users[0]);
+    const user = service.getUser();
+    expect(user).toEqual(users[0]);
   });
 });
